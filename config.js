b7e4f52b0ef2d83771a554509814390fae07ebff
IsmailAli225 <106046159+IsmailAli225@users.noreply.github.com>
2026-09-10 13:19:10 +1000

    Fix formatting of apiBase in VOTING_CONFIG

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.VOTING_CONFIG={title:"Community Association Voting Hub",apiBase:"https://community-voting-api.ia-apps.workers.dev"};
+window.VOTING_CONFIG={title:"Community Association Voting Hub",apiBase: "https://community-voting-api.ia-apps.workers.dev"};
